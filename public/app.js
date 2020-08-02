9183c3cc6b15bccd86c89a181aa1015799bb3586
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-02 16:34:34 -0500

    Deleted content from index.html to recreate bootsrap css

--- public/app.js
+++ public/app.js
@@ -1,1 +1,26 @@
-console.log(firebase)
+const auth=firebase.auth()
+const provider=new firebase.auth.GoogleAuthProvider();
+
+
+const loginBtn=document.querySelector("#loginButton")
+const logoutBtn=document.querySelector("#logoutButton")
+
+loginBtn.addEventListener("click",()=>{
+    auth.signInWithPopup(provider)
+})
+
+logoutBtn.addEventListener("click",()=>{
+    auth.signOut();
+})
+
+auth.onAuthStateChanged((user)=>{
+    if(user){
+        console.log("User is present")
+        loginBtn.hidden=true
+        logoutBtn.hidden=false
+    }else{
+        console.log("User loged out")
+        loginBtn.hidden=false
+        logoutBtn.hidden=true
+    }
+})
